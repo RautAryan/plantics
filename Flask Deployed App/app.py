b33bf16f91d62d74b8553f62e521bd352a7021c8
RautAryan <aryanraut9529@gmail.com>
2026-04-03 22:10:40 +0530

    fixed clean app.py final

--- Flask Deployed App/app.py
+++ Flask Deployed App/app.py
@@ -1,33 +1,120 @@
+# import os
+# from flask import Flask, redirect, render_template, request
+# from PIL import Image
+# import torchvision.transforms.functional as TF
+# import CNN
+# import numpy as np
+# import torch
+# import pandas as pd
+# import gdown
+#
+#
+# disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+# supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
+#
+# # model = CNN.CNN(39)
+# # model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+# # model.eval()
+#
+# model = CNN.CNN(39)
+#
+# model_path = "plant_disease_model_1_latest.pt"
+#
+# if not os.path.exists(model_path):
+#     url = "https://drive.google.com/uc?id=1itvjp7C5pC8gGmY5Nuj4wAxcquvzUbO0"
+#     gdown.download(url, model_path, quiet=False)
+#
+# model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+# model.eval()
+#
+#
+# def prediction(image_path):
+#     image = Image.open(image_path)
+#     image = image.resize((224, 224))
+#     input_data = TF.to_tensor(image)
+#     input_data = input_data.view((-1, 3, 224, 224))
+#     output = model(input_data)
+#     output = output.detach().numpy()
+#     index = np.argmax(output)
+#     return index
+#
+# os.makedirs('static/uploads', exist_ok=True)
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home_page():
+#     return render_template('home.html')
+#
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact-us.html')
+#
+# @app.route('/index')
+# def ai_engine_page():
+#     return render_template('index.html')
+#
+# @app.route('/mobile-device')
+# def mobile_device_detected_page():
+#     return render_template('mobile-device.html')
+#
+# @app.route('/submit', methods=['GET', 'POST'])
+# def submit():
+#     if request.method == 'POST':
+#         image = request.files['image']
+#         filename = image.filename
+#         file_path = os.path.join('static/uploads', filename)
+#         image.save(file_path)
+#         print(file_path)
+#         pred = prediction(file_path)
+#         title = disease_info['disease_name'][pred]
+#         description =disease_info['description'][pred]
+#         prevent = disease_info['Possible Steps'][pred]
+#         image_url = disease_info['image_url'][pred]
+#         supplement_name = supplement_info['supplement name'][pred]
+#         supplement_image_url = supplement_info['supplement image'][pred]
+#         supplement_buy_link = supplement_info['buy link'][pred]
+#         return render_template('submit.html' , title = title , desc = description , prevent = prevent ,
+#                                image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+#
+# @app.route('/market', methods=['GET', 'POST'])
+# def market():
+#     return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
+#                            supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+#
+# if __name__ == '__main__':
+#     # app.run(debug=True)  this is changed by me so that i can hos my website as webapp
+#     app.run(host='0.0.0.0', port=10000)
+
 import os
-from flask import Flask, redirect, render_template, request
-from PIL import Image
-import torchvision.transforms.functional as TF
-import CNN
+import gdown
 import numpy as np
 import torch
 import pandas as pd
-import gdown
+from flask import Flask, render_template, request
+from PIL import Image
+import torchvision.transforms.functional as TF
+import CNN
 
+# Load CSV files
+disease_info = pd.read_csv('disease_info.csv', encoding='cp1252')
+supplement_info = pd.read_csv('supplement_info.csv', encoding='cp1252')
 
-disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
-supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
-
-# model = CNN.CNN(39)
-# model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
-# model.eval()
-
+# Create model
 model = CNN.CNN(39)
 
+# Model path
 model_path = "plant_disease_model_1_latest.pt"
 
+# Download model if not exists
 if not os.path.exists(model_path):
     url = "https://drive.google.com/uc?id=1itvjp7C5pC8gGmY5Nuj4wAxcquvzUbO0"
     gdown.download(url, model_path, quiet=False)
 
+# Load model
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 model.eval()
 
-
+# Prediction function
 def prediction(image_path):
     image = Image.open(image_path)
     image = image.resize((224, 224))
@@ -38,7 +125,10 @@ def prediction(image_path):
     index = np.argmax(output)
     return index
 
+# Ensure upload folder exists
 os.makedirs('static/uploads', exist_ok=True)
+
+# Flask app
 app = Flask(__name__)
 
 @app.route('/')
@@ -64,23 +154,39 @@ def submit():
         filename = image.filename
         file_path = os.path.join('static/uploads', filename)
         image.save(file_path)
-        print(file_path)
+
         pred = prediction(file_path)
+
         title = disease_info['disease_name'][pred]
-        description =disease_info['description'][pred]
+        description = disease_info['description'][pred]
         prevent = disease_info['Possible Steps'][pred]
         image_url = disease_info['image_url'][pred]
+
         supplement_name = supplement_info['supplement name'][pred]
         supplement_image_url = supplement_info['supplement image'][pred]
         supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
-                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
 
-@app.route('/market', methods=['GET', 'POST'])
+        return render_template(
+            'submit.html',
+            title=title,
+            desc=description,
+            prevent=prevent,
+            image_url=image_url,
+            pred=pred,
+            sname=supplement_name,
+            simage=supplement_image_url,
+            buy_link=supplement_buy_link
+        )
+
+@app.route('/market')
 def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+    return render_template(
+        'market.html',
+        supplement_image=list(supplement_info['supplement image']),
+        supplement_name=list(supplement_info['supplement name']),
+        disease=list(disease_info['disease_name']),
+        buy=list(supplement_info['buy link'])
+    )
 
 if __name__ == '__main__':
-    # app.run(debug=True)  this is changed by me so that i can hos my website as webapp
     app.run(host='0.0.0.0', port=10000)
